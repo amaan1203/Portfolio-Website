e5391b9f524310fd7104ab38871639f954721c75
amaan1203 <127188069+amaan1203@users.noreply.github.com>
2023-06-01 17:30:21 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {fontFamily: {
+      mullish: ["Mulish", "sans-serif"],
+      mullish2:["Be Vietnam Pro","sans-serif"],
+      railway2:["Bruno Ace SC", "cursive"],
+      raleway1: ["Quicksand", "sans-serif"],
+    },
+    colors: {
+      grey:"#808080",
+      bgOrange: "#e84949",
+      purple:"#4e45d5",
+    },
+
+    },
+  },
+  plugins: [],
+}
+
